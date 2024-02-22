1569fb96f85e47810f70819fbd6a1e5930fc42cb
陳亮瑜 <liangyu@chenliangyudeMacBook-Pro.local>
2024-02-22 21:08:55 +0800

    ### W01-P2: Implement add function

---
+++ demo/w01-demo_31/js/w01_31.js
@@ -0,0 +1,52 @@
+//alert('hello'); 測試javascript是否可以用
+
+//const常數；querySelector查詢；‘’常數
+const userInput = document.querySelector('#input-number');
+
+const addBtn = document.querySelector('#btn-add');
+const subtractBtn = document.querySelector('#btn-subtract');
+const multiplyBtn = document.querySelector('#btn-multiply');
+const divideBtn = document.querySelector('#btn-divide');
+
+const currentCalculationOutput = document.querySelector('#current-calculation');
+const currentResultOutput = document.querySelector('#current-result');
+
+//‘輸出字串’
+// console.log('userInput', userInput);
+// console.log('addBtn', addBtn);
+// console.log('subtractBtn', subtractBtn);
+//console.log('multiplyBtn', multiplyBtn);
+console.log('divideBtn', divideBtn);
+// console.log('currentCalculationOutput', currentCalculationOutput);
+// console.log('currentResultOutput', currentResultOutput);
+
+const defaultResult = 0;
+//數值會改變，設let變數
+let currentResult = defaultResult;
+
+function getUserInput() {
+  // return userInput.value
+  return parseInt(userInput.value);
+}
+
+function outputResult(result, text) {
+  currentCalculationOutput.textContent = text;
+  currentResultOutput.textContent = result;
+}
+
+//add程式
+//operand運算元
+function add() {
+  const operand1 = currentResult;
+  const operand2 = getUserInput();
+  //console.log(operand1, operand2, operand1 + operand2);
+  //相加
+  currentResult = operand1 + operand2;
+  // `字串裡面可以夾變數`
+  const calTest = `${operand1} + ${operand2}`;
+  console.log(`${operand1} + ${operand2} = ${currentResult}`);
+  outputResult(currentResult, calTest);
+}
+
+//mouse click後交給add程式處理
+addBtn.addEventListener('click', add);
