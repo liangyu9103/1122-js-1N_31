9e3cf4ffd83b97e7bc0fb5a90e676cc853319309
陳亮瑜 <liangyu@chenliangyudeMacBook-Pro.local>
2024-03-26 20:03:22 +0800

    ### W05-P4: Display Menu buttons from categories array (do it by yourself)

--- demo/w05-menu_31/script_31.js
+++ demo/w05-menu_31/script_31.js
@@ -1,8 +1,12 @@
 //多個匯入需要加大括號,且menu名稱可以改
+import category from './btndata_31.js';
 import menu from './data_31.js';
+
 //找到section-center的位置
 const sectionCenter = document.querySelector('.section-center');
+const btnContainer = document.querySelector('.btn-container');
 console.log('menu', menu);
+console.log('category', category);
 
 //menu.map((item):轉成陣列把每一個item走一遍
 const displayMenuItems = (menu) => {
@@ -33,17 +37,30 @@ const displayMenuItems = (menu) => {
     })
     //把陣列資料join起來變字串
     .join('');
-  console.log('displayMenu', displayMenu);
+  //console.log('displayMenu', displayMenu);
   //把資料插進sectionCenter
   sectionCenter.innerHTML = displayMenu;
 };
 
 //回家作業
-const category = ['all', 'breakfast', 'lunch', 'dinner', 'shakes'];
-const displayMenuButtons = () => {};
+const displayMenuButtons = (category) => {
+  let displayButtons = category
+    .map((btn) => {
+      //const category = ['all', 'breakfast', 'lunch', 'dinner', 'shakes'];
+      const { id, text } = btn;
+      return `
+        <div class="btn-container">
+        <button type="button" class="filter-btn" data-id="all">${text}</button>
+      </div>
+            `;
+    })
+    .join('');
+  console.log('displayButtons', displayButtons);
+  btnContainer.innerHTML = displayButtons;
+};
 
-//dom讀進來後,呼叫一個程式; caal back fn:() => {}:資料抓回來之後通知
+//dom讀進來後,呼叫一個程式; call back fn:() => {}:資料抓回來之後通知
 window.addEventListener('DOMContentLoaded', () => {
   displayMenuItems(menu);
-  displayMenuButtons();
+  displayMenuButtons(category);
 });
