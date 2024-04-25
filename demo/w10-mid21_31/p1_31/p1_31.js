7a3c6e4506701a95b0b2cdaa48755183b6559df5
liangyu9103 <liangyu9103@gmail.com>
2024-04-25 19:15:21 +0800

    ### W10-P1: In Supabase, create product_xx table with 8 product data

---
+++ demo/w10-mid21_31/p1_31/p1_31.js
@@ -0,0 +1,11 @@
+import { products_31, all_products_31 } from './p1_data_31.js';
+
+const productContainer = document.querySelector('.products-container');
+
+console.log('products_31', products_31);
+
+const DisplayProducts = (products) => {};
+
+document.addEventListener('DOMContentLoaded', () => {
+  DisplayProducts(products_31);
+});
