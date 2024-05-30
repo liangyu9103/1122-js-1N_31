201252e8f3c05f2f1c4e4bee884631b97bbc681c
liangyu9103 <liangyu9103@gmail.com>
2024-05-30 20:39:08 +0800

    ### W15-P3: Implement company filter functions, test for each company using direct filtering of the products containing company data

--- demo/w15_product_31/js/products_supa_31 copy.js
+++ demo/w15_product_31/js/products_supa_31 copy.js
@@ -6,39 +6,24 @@ import { _supabase } from './clientSupabase_31.js';
 //console.log('_supabase', _supabase);
 
 let products2_31 = [];
-//console.log('product_31', products2_31);
 
-const productContainer = document.querySelector('.products-container');
-const companyBtns = document.querySelectorAll('.company-btn');
 //*:指到table裡面所有的資料
 const getProductsSupabase_31 = async () => {
   try {
     let { data, error } = await _supabase
       .from('products2_31')
       .select('*, company2_31(*)'); //除了products2_31裡所有的資料，還要company2_31(*）裡所有的資料
-    //console.log('products data', data);
+    console.log('products data', data);
     return data;
   } catch (error) {
     console.log(error);
   }
 };
 
-companyBtns.forEach((btn) => {
-  btn.addEventListener('click', async (e) => {
-    const companyName = e.currentTarget.dataset.id;
-    console.log('companyName', companyName);
-    const products = await getProductsSupabase_31();
-    if (companyName === 'all') {
-      products2_31 = products;
-    } else {
-      products2_31 = products.filter(
-        (product) => product.company2_31.name === companyName
-      );
-    }
-    console.log(`${companyName} products`, products2_31);
-    displayProducts(products2_31);
-  });
-});
+const productContainer = document.querySelector('.products-container');
+const companyBtns = document.querySelectorAll('.company-btn');
+
+console.log('product_31', products2_31);
 
 const displayProducts = (products) => {
   /*把products陣列用map轉到另外一個productsContent陣列*/
@@ -63,6 +48,29 @@ const displayProducts = (products) => {
     .join('');
   productContainer.innerHTML = productsContent;
 };
+
+companyBtns.forEach((btn) => {
+  btn.addEventListener('click', async (e) => {
+    const companyName = e.currentTarget.dataset.id;
+    console.log('companyName', companyName);
+    if (companyName === 'all') {
+      products2_31 = await getProductsSupabase_31();
+    } else {
+      let { data: company, error1 } = await _supabase
+        .from('company2_31')
+        .select('id')
+        .eq('name', companyName);
+      console.log('company data', company[0].id);
+      let { data, error } = await _supabase
+        .from('products2_31')
+        .select('*')
+        .eq('companyId', company[0].id);
+      console.log(`${companyName} products`, data);
+      products2_31 = data;
+    }
+    displayProducts(products2_31);
+  });
+});
 
 /*async:非同步 */
 document.addEventListener('DOMContentLoaded', async () => {
