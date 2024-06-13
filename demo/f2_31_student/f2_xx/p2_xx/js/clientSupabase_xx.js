9a5b8369079ba0cf582a506867e86c6c31a3531d
liangyu9103 <liangyu9103@gmail.com>
2024-06-13 18:49:14 +0800

    #### P1-1: Show Your Github Repo for all md and demo

---
+++ demo/f2_31_student/f2_xx/p2_xx/js/clientSupabase_xx.js
@@ -0,0 +1,4 @@
+const SUPABASE_URL = '';
+const SUPABASE_ANON_KEY = '';
+
+export const _supabase = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
---
+++ demo/f2_31_student/f2_xx/p2_xx/js/p2_xx.js
@@ -0,0 +1,5 @@
+import { _supabase } from './clientSupabase_xx.js';
+
+let products_xx = [];
+
+document.addEventListener('DOMContentLoaded', async () => {});
